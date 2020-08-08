ebf1134e07b7fcb8934fbc29ae255956ab69de51
Hayet.BHH <66040083+hayet-ben-haj-hamida@users.noreply.github.com>
2020-08-08 15:06:48 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
+## compute an inverse of squared matrix
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  M<-NULL
+  set<-function(z){
+    x<<-z
+    M<<-NULL
+  }
+  get<-function() x
+ # setinverse <- aperm(matrix)*(1/det(matrix))
+  setinverse <- solve(x)
+  getinverse <- function() M
+  list(set = set, get = get,
+       setinverse= setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
+         M<-x$getinverse() 
+ if(det(x)!= 0){message("getting cached matrix inverse")
+   return(M)}
+ data <- x$get()
+ M <- solve(data,...)
+x$setinverse(M)
+ M
         ## Return a matrix that is the inverse of 'x'
 }
